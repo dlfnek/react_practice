3b58b7ca3175d4c3d97c5edd1597d971050be917
godkorea <dlfnek3@gmail.com>
2024-07-14 18:48:38 +0900

    React Route path practice

--- react-app-prac/src/App.js
+++ react-app-prac/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie" element={<Detail />} />
+        {/* :id는 url 파라미터 값을 의미한다.
+        단, 앞에 콜론을 붙여주어야만 변수로 사용할 수 있음. */}
+        <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
